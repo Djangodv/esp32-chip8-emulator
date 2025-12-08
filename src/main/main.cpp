f099a0244ad7a530c907bc03e3c89a9c17a72e06
Djangodv <dd.manders@protonmail.com>
2025-12-08 14:40:36 +0000

    feat: added functionality for drawing pixels

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,6 +1,8 @@
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "Ili9341Display.hpp"
+#include <esp_random.h>
 
 static const char* TAG = "MAIN";
 
@@ -8,7 +10,12 @@ extern "C" void app_main(void)
 {
 
     ESP_LOGI(TAG, "Started running 'main' task");
-    while (true) {
-		vTaskDelay(pdMS_TO_TICKS(1000));
-    }
+
+	Ili9341Display display(13, 14, 15, 2, -1, 27); // MOSI, SCLK, CS, DC, RST, BCKL (BL on GPIO 27)
+    display.init();
+	ESP_LOGI(TAG, "Succesfully initialized display");
+	// display.diagnostics();
+
+	display.fillScreen(display.rbg565(0, 0, 0)); // clear the screen
+
 }
